569c4d4fd3ebd39522a8b6eb6cd73b37ab96c66b
ArthurMelot <arthur.melot@gmail.com>
2024-11-12 19:14:00 +0100

    Modified budget constraint

--- model.jl
+++ model.jl
@@ -1,7 +1,7 @@
 using JuMP
 using HiGHS
 
-function solve_OptVax1(n::Int64, m::Int64, A::Array{Int64, 2}, Q::Int64, C::Vector{Int64}, q::Vector{Int64}, f::Vector{Int64}, p::Int64, B::Float64, R::Vector{Float64}, J_prime::Vector{Int64}, M::Int64) 
+function solve_OptVax1(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64, 2}, Q::Int64, C::Vector{Int64}, q::Vector{Int64}, f::Vector{Int64}, p::Int64, B::Float64, R::Vector{Float64}, J_prime::Vector{Int64}, M::Int64) 
     
     I = 1:m
     J = 1:n
@@ -55,9 +55,9 @@ function solve_OptVax1(n::Int64, m::Int64, A::Array{Int64, 2}, Q::Int64, C::Vect
     # 6. Coverage and MMT-VC relationships
     for l in I
         for k in 1:M
-            @constraint(model, sum(z[l, i, k] for i in N) <= y[l])
-            @constraint(model, sum(z[l, i, k] for i in N) <= delta[k])
-            @constraint(model, sum(z[l, i, k] for i in N) >= y[l] + delta[k] - 1)
+            @constraint(model, sum(z[l+n, i, k] for i in N) <= y[l])
+            @constraint(model, sum(z[l+n, i, k] for i in N) <= delta[k])
+            @constraint(model, sum(z[l+n, i, k] for i in N) >= y[l] + delta[k] - 1)
         end
     end
 
@@ -75,7 +75,7 @@ function solve_OptVax1(n::Int64, m::Int64, A::Array{Int64, 2}, Q::Int64, C::Vect
     end
 
     # 10. Budget constraint
-    @constraint(model, (sum(delta[k] for k in 1:M) * p) + sum(y[i] * f[i] for i in I) <= B)
+    @constraint(model, (sum(z[i,j,k]*D[i,j] for k in 1:M, i in N, j in N)) + (sum(delta[k] for k in 1:M) * p) + sum(y[i] * f[i] for i in I) <= B)
 
     # 11. Total MMT limit
     @constraint(model, sum(delta[k] for k in 1:M) <= M)
@@ -86,7 +86,7 @@ function solve_OptVax1(n::Int64, m::Int64, A::Array{Int64, 2}, Q::Int64, C::Vect
 end
 
 
-function solve_OptVax2(n::Int64, m::Int64, A::Array{Int64, 2}, Q::Int64, C::Vector{Int64}, q::Vector{Int64}, f::Vector{Int64}, p::Int64, B::Float64, R::Vector{Float64}, J_prime::Vector{Int64}, M::Int64) 
+function solve_OptVax2(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64, 2}, Q::Int64, C::Vector{Int64}, q::Vector{Int64}, f::Vector{Int64}, p::Int64, B::Float64, R::Vector{Float64}, J_prime::Vector{Int64}, M::Int64) 
     
     I = 1:m
     J = 1:n
@@ -140,9 +140,9 @@ function solve_OptVax2(n::Int64, m::Int64, A::Array{Int64, 2}, Q::Int64, C::Vect
     # 6. Coverage and MMT-VC relationships
     for l in I
         for k in 1:M
-            @constraint(model, sum(z[l, i, k] for i in N) <= y[l])
-            @constraint(model, sum(z[l, i, k] for i in N) <= delta[k])
-            @constraint(model, sum(z[l, i, k] for i in N) >= y[l] + delta[k] - 1)
+            @constraint(model, sum(z[l+n, i, k] for i in N) <= y[l])
+            @constraint(model, sum(z[l+n, i, k] for i in N) <= delta[k])
+            @constraint(model, sum(z[l+n, i, k] for i in N) >= y[l] + delta[k] - 1)
         end
     end
 
@@ -160,7 +160,7 @@ function solve_OptVax2(n::Int64, m::Int64, A::Array{Int64, 2}, Q::Int64, C::Vect
     end
 
     # 10. Budget constraint
-    @constraint(model, (sum(delta[k] for k in 1:M) * p) + sum(y[i] * f[i] for i in I) <= B)
+    @constraint(model, (sum(z[i,j,k]*D[i,j] for k in 1:M, i in N, j in N)) + (sum(delta[k] for k in 1:M) * p) + sum(y[i] * f[i] for i in I) <= B)
 
     # 11. Total MMT limit
     @constraint(model, sum(delta[k] for k in 1:M) <= M)
