c5d378382deaf89b3016277d0ac3c3e18ff0c241
TheoHanon <99546301+TheoHanon@users.noreply.github.com>
2024-11-14 23:59:06 +0100

    Add subtour elimination constraints to optimization model

--- model.jl
+++ model.jl
@@ -18,11 +18,34 @@ function solve_OptVax1(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64,
     @variable(model, z[i in N, j in N, k in 1:M], Bin)
     @variable(model, delta[k in 1:M], Bin)
 
+    
+
     # Objective function
 
     @objective(model, Max, sum(q[j] * x[j] for j in J))
 
     # Constraints
+
+
+    ## SUBTOUR ELIMINATION CONSTRAINTS---------------
+    @variable(model, beta[i in N], Int)
+
+    for i in I
+        @constraint(model, beta[i+n] == y[i])
+    end
+
+    for j in J
+        @constraint(model, beta[j] >= 2)
+        @constraint(model, beta[j] <= n)
+    end
+
+    for k in 1:M, i in N, j in J
+        if i != j
+            @constraint(model, beta[i] - beta[j] + (n-1)*z[i,j,k] <= n-2)
+        end
+    end
+
+    #------------------------------------------------
 
     # 1. Total VC locations must equal 1
     @constraint(model, sum(y[i] for i in I) == 1)
@@ -32,11 +55,10 @@ function solve_OptVax1(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64,
         @constraint(model, v[j] == sum(A[i, j] * y[i] for i in I))
         @constraint(model, x[j] <= u[j] + v[j])
         @constraint(model, u[j] + v[j] <=1.0)
-
     end
 
     #  3. Routing constraints
-    for j in J
+    for j in J 
         @constraint(model, sum(z[i, j, k] for k in 1:M, i in N) == u[j])
     end
 
@@ -53,12 +75,8 @@ function solve_OptVax1(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64,
     end
 
     # 5.5. Avoid double arrows
-    for i in N
-        for j in N
-            for k in 1:M
-                @constraint(model, z[i,j,k]+z[j,i,k] <= 1)
-            end
-        end
+    for i in N, k in 1:M            
+        @constraint(model, z[i,i,k] == 0)
     end
 
     # 6. Coverage and MMT-VC relationships
@@ -85,6 +103,7 @@ function solve_OptVax1(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64,
 
     # 10. Budget constraint
     @constraint(model, (sum(z[i,j,k]*D[i,j] for k in 1:M, i in N, j in N)) + (sum(delta[k] for k in 1:M) * p) + sum(y[i] * f[i] for i in I) <= B)
+
 
     # 11. Total MMT limit
     @constraint(model, sum(delta[k] for k in 1:M) <= M)
@@ -118,6 +137,26 @@ function solve_OptVax2(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64,
 
     # Constraints
 
+    ## SUBTOUR ELIMINATION CONSTRAINTS---------------
+    @variable(model, beta[i in N], Int)
+
+    for i in I
+        @constraint(model, beta[i+n] == y[i])
+    end
+
+    for j in J
+        @constraint(model, beta[j] >=2)
+        @constraint(model, beta[j] <= n)
+    end
+
+    for k in 1:M, i in N, j in J
+        if i != j
+            @constraint(model, beta[i] - beta[j] + (n-1)*z[i,j,k] <= n-2)
+        end
+    end
+
+    #------------------------------------------------
+
     # 1. Total VC locations must equal 1
     @constraint(model, sum(y[i] for i in I) == 1)
     
@@ -125,25 +164,29 @@ function solve_OptVax2(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64,
     for j in J
         @constraint(model, v[j] == sum(A[i, j] * y[i] for i in I))
         @constraint(model, x[j] <= u[j] + v[j])
-        @constraint(model, u[j] + v[j] <=1)
-
+        @constraint(model, u[j] + v[j] <=1.0)
     end
 
     #  3. Routing constraints
-    for j in J
+    for j in J 
         @constraint(model, sum(z[i, j, k] for k in 1:M, i in N) == u[j])
     end
 
     # 4.  Flow conservation constraints
     for k in 1:M
         for h in N
-            @constraint(model, sum(z[i, h, k] for i in N) - sum(z[h, j, k] for j in N) == 0)
+            @constraint(model, sum(z[i, h, k] for i in N) - sum(z[h, j, k] for j in N) == 0.0)
         end
     end
 
     # 5. Capacity constraints for MMTs
     for k in 1:M
         @constraint(model, sum(q[j] * z[i,j,k] for j in J, i in N) <= Q)
+    end
+
+    # 5.5. Avoid double arrows
+    for i in N, k in 1:M            
+        @constraint(model, z[i,i,k] == 0)
     end
 
     # 6. Coverage and MMT-VC relationships
@@ -171,9 +214,9 @@ function solve_OptVax2(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64,
     # 10. Budget constraint
     @constraint(model, (sum(z[i,j,k]*D[i,j] for k in 1:M, i in N, j in N)) + (sum(delta[k] for k in 1:M) * p) + sum(y[i] * f[i] for i in I) <= B)
 
+
     # 11. Total MMT limit
     @constraint(model, sum(delta[k] for k in 1:M) <= M)
-
     # 12. Additionnal constraints on the number of MMTs
     
     @constraint(model, sum(delta[k] for k in 1:M) >= sum(u[j]*q[j]/Q for j in J))
