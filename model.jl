cde08b0aedfdc948626ffe5f8f1331d34b220f92
ArthurMelot <arthur.melot@gmail.com>
2024-11-16 11:49:48 +0100

    Code cleaning and vectorization

--- model.jl
+++ model.jl
@@ -1,211 +1,153 @@
 using JuMP
 using HiGHS
 
-function solve_OptVax1(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64, 2}, Q::Int64, C::Vector{Int64}, q::Vector{Int64}, f::Vector{Int64}, p::Int64, B::Float64, R::Vector{Float64}, J_prime::Vector{Int64}, M::Int64) 
-    
+function solve_OptVax1(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64, 2}, Q::Int64, C::Vector{Int64}, q::Vector{Int64}, f::Vector{Int64}, p::Int64, B::Float64, R::Vector{Float64}, J_prime::Vector{Int64}, M::Int64)
     I = 1:m
     J = 1:n
-    N = 1:(n+m)
- 
+    N = 1:(n + m)
+    
     model = Model(HiGHS.Optimizer)
 
-    # Creation of the variables
-    
-    @variable(model, y[i in I], Bin)
-    @variable(model, u[j in J], Bin)
-    @variable(model, v[j in J], Bin)
-    @variable(model, z[i in N, j in N, k in 1:M], Bin)
-    @variable(model, delta[k in 1:M], Bin)
+    # Variables
+    @variable(model, y[I], Bin)
+    @variable(model, u[J], Bin)
+    @variable(model, v[J], Bin)
+    @variable(model, z[N, N, 1:M], Bin)
+    @variable(model, delta[1:M], Bin)
+    @variable(model, beta[N], Int)
 
-    
+    #Expression to avoid multiple computations
+    @expression(model, sum_uq, sum(u .* q))
 
     # Objective function
-    @objective(model, Max, sum(q[j] * (u[j] + v[j]) for j in J))
+    @objective(model, Max, sum_uq + sum(q .* v))
 
     # Constraints
 
+    # Subtour elimination constraints
+    @constraint(model, beta[J] .>= q)
+    @constraint(model, beta[J] .<= Q)
+    valid_pairs = [(i, j) for i in N, j in J if i != j]
+    @constraint(model, [k = 1:M, (i, j) in valid_pairs], beta[i] - beta[j] >= q[j] - (1 - z[i, j, k]) * Q)
 
-    ## SUBTOUR ELIMINATION CONSTRAINTS---------------
-    @variable(model, beta[i in N], Int)
+    # Total VC locations must equal 1
+    @constraint(model, sum(y) == 1)
 
-    for j in J
-        @constraint(model, beta[j] >= q[j])
-        @constraint(model, beta[j] <= Q)
-    end
+    # Coverage and locality vaccination relationships
+    @constraint(model, [j in J], v[j] == sum(A[i, j] * y[i] for i in I))
+    @constraint(model, u .+ v .<= 1.0)
 
-    for k in 1:M, i in N, j in J
-        if i != j
-            @constraint(model, beta[i] - beta[j] >=  q[j] - (1-z[i,j,k]) * Q)
-        end
-    end
+    # Routing constraints
+    @constraint(model, [j in J], sum(z[i, j, k] for k in 1:M, i in N) == u[j])
 
-    #------------------------------------------------
+    # Flow conservation constraints
+    @constraint(model, [k=1:M, h in N], sum(z[i, h, k] for i in N) - sum(z[h, j, k] for j in N) == 0.0)
 
-    # 1. Total VC locations must equal 1
-    @constraint(model, sum(y[i] for i in I) == 1)
-    
-    # 2. Coverage and locality vaccination relationships
-    for j in J
-        @constraint(model, v[j] == sum(A[i, j] * y[i] for i in I))
-        @constraint(model, u[j] + v[j] <=1.0)
-    end
+    # Avoid double arrows
+    @constraint(model, [i in N, k in 1:M], z[i, i, k] == 0)
 
-    #  3. Routing constraints
-    for j in J 
-        @constraint(model, sum(z[i, j, k] for k in 1:M, i in N) == u[j])
-    end
+    # Coverage and MMT-VC relationships
+    @expression(model, sum_z[l in I, k in 1:M], sum(z[l + n, i, k] for i in N))
+    @constraint(model, [l in I, k in 1:M], sum_z[l, k] <= y[l])
+    @constraint(model, [l in I, k in 1:M], sum_z[l, k] <= delta[k])
+    @constraint(model, [l in I, k in 1:M], sum_z[l, k] >= y[l] + delta[k] - 1)
 
-    # 4.  Flow conservation constraints
-    for k in 1:M
-        for h in N
-            @constraint(model, sum(z[i, h, k] for i in N) - sum(z[h, j, k] for j in N) == 0.0)
-        end
-    end
+    # Ensure z is only active if MMT is used
+    @constraint(model, [i in N, j in N], z[i, j, :] .<= delta)
 
-    # 5. Capacity constraints for MMTs
-    # for k in 1:M
-    #     @constraint(model, sum(q[j] * z[i,j,k] for j in J, i in N) <= Q)
-    # end
+    # Cost constraint
+    @constraint(model, sum_uq <= sum(C .* y))
 
-    # 5.5. Avoid double arrows
-    for i in N, k in 1:M            
-        @constraint(model, z[i,i,k] == 0)
-    end
+    # Enforced coverage for specific localities
+    @constraint(model, [j in J_prime], u[j] + v[j] == 1)
 
-    # 6. Coverage and MMT-VC relationships
-    for l in I
-        for k in 1:M
-            @constraint(model, sum(z[l+n, i, k] for i in N) <= y[l])
-            @constraint(model, sum(z[l+n, i, k] for i in N) <= delta[k])
-            @constraint(model, sum(z[l+n, i, k] for i in N) >= y[l] + delta[k] - 1)
-        end
-    end
-
-    # 7. Ensure z is only active if MMT is used
-    for i in N, j in N, k in 1:M
-        @constraint(model, z[i, j, k] <= delta[k])
-    end
-
-    # 8. Cost constraint
-    @constraint(model, sum(q[j] * u[j] for j in J) <= sum(C[i] * y[i] for i in I))
-
-    # 9. Enforced coverage for specific localities
-    for j in J_prime
-        @constraint(model, u[j] + v[j] == 1)
-    end
-
-    # 10. Budget constraint
-    @constraint(model, (sum(z[i,j,k]*D[i,j] for k in 1:M, i in N, j in N)) + (sum(delta[k] for k in 1:M) * p) + sum(y[i] * f[i] for i in I) <= B)
+    # Budget constraint
+    @constraint(model,
+        sum(D .* sum(z[:,:,k] for k in 1:M)) +
+        sum(delta) * p +
+        sum(y .* f) <= B
+    )
 
     optimize!(model)
-
     return model
 end
 
 
+
 function solve_OptVax2(n::Int64, m::Int64, D::Array{Float64, 2}, A::Array{Int64, 2}, Q::Int64, C::Vector{Int64}, q::Vector{Int64}, f::Vector{Int64}, p::Int64, B::Float64, R::Vector{Float64}, J_prime::Vector{Int64}, M::Int64) 
-    
     I = 1:m
     J = 1:n
-    N = 1:(n+m)
- 
+    N = 1:(n + m)
+    
     model = Model(HiGHS.Optimizer)
 
-    # Creation of the variables
-    
-    @variable(model, y[i in I], Bin)
-    @variable(model, x[j in J], Bin)
-    @variable(model, u[j in J], Bin)
-    @variable(model, v[j in J], Bin)
-    @variable(model, z[i in N, j in N, k in 1:M], Bin)
-    @variable(model, delta[k in 1:M], Bin)
+    # Variables
+    @variable(model, y[I], Bin)
+    @variable(model, u[J], Bin)
+    @variable(model, v[J], Bin)
+    @variable(model, z[N, N, 1:M], Bin)
+    @variable(model, delta[1:M], Bin)
+    @variable(model, beta[N], Int)
+
+    #Expression to avoid multiple computations
+    @expression(model, sum_uq, sum(u .* q))
 
     # Objective function
-
-    @objective(model, Max, sum(q[j] * x[j] for j in J))
+    @objective(model, Max, sum_uq + sum(q .* v))
 
     # Constraints
 
-    ## SUBTOUR ELIMINATION CONSTRAINTS---------------
-    @variable(model, beta[i in N], Int)
+    # Subtour elimination constraints
+    @constraint(model, beta[J] .>= q)
+    @constraint(model, beta[J] .<= Q)
+    valid_pairs = [(i, j) for i in N, j in J if i != j]
+    @constraint(model, [k = 1:M, (i, j) in valid_pairs], beta[i] - beta[j] >= q[j] - (1 - z[i, j, k]) * Q)
 
-    for i in I
-        @constraint(model, beta[i+n] == y[i])
-    end
+    # Total VC locations must equal 1
+    @constraint(model, sum(y) == 1)
 
-    for j in J
-        @constraint(model, beta[j] >=2)
-        @constraint(model, beta[j] <= n)
-    end
+    # Coverage and locality vaccination relationships
+    @constraint(model, [j in J], v[j] == sum(A[i, j] * y[i] for i in I))
+    @constraint(model, u .+ v .<= 1.0)
 
-    for k in 1:M, i in N, j in J
-        if i != j
-            @constraint(model, beta[i] - beta[j] + (n-1)*z[i,j,k] <= n-2)
-        end
-    end
+    # Routing constraints
+    @constraint(model, [j in J], sum(z[i, j, k] for k in 1:M, i in N) == u[j])
 
-    #------------------------------------------------
+    # Flow conservation constraints
+    @constraint(model, [k=1:M, h in N], sum(z[i, h, k] for i in N) - sum(z[h, j, k] for j in N) == 0.0)
 
-    # 1. Total VC locations must equal 1
-    @constraint(model, sum(y[i] for i in I) == 1)
-    
-    # 2. Coverage and locality vaccination relationships
-    for j in J
-        @constraint(model, v[j] == sum(A[i, j] * y[i] for i in I))
-        @constraint(model, x[j] <= u[j] + v[j])
-        @constraint(model, u[j] + v[j] <=1.0)
-    end
+    # Avoid double arrows
+    @constraint(model, [i in N, k in 1:M], z[i, i, k] == 0)
 
-    #  3. Routing constraints
-    for j in J 
-        @constraint(model, sum(z[i, j, k] for k in 1:M, i in N) == u[j])
-    end
+    # Coverage and MMT-VC relationships
+    @expression(model, sum_z[l in I, k in 1:M], sum(z[l + n, i, k] for i in N))
+    @constraint(model, [l in I, k in 1:M], sum_z[l, k] <= y[l])
+    @constraint(model, [l in I, k in 1:M], sum_z[l, k] <= delta[k])
+    @constraint(model, [l in I, k in 1:M], sum_z[l, k] >= y[l] + delta[k] - 1)
 
-    # 4.  Flow conservation constraints
-    for k in 1:M
-        for h in N
-            @constraint(model, sum(z[i, h, k] for i in N) - sum(z[h, j, k] for j in N) == 0.0)
-        end
-    end
+    # Ensure z is only active if MMT is used
+    @constraint(model, [i in N, j in N, k in 1:M], z[i, j, :] .<= delta)
 
-    # 5. Capacity constraints for MMTs
-    for k in 1:M
-        @constraint(model, sum(q[j] * z[i,j,k] for j in J, i in N) <= Q)
-    end
+    # Cost constraint
+    @constraint(model, sum_uq <= sum(C .* y))
 
-    # 5.5. Avoid double arrows
-    for i in N, k in 1:M            
-        @constraint(model, z[i,i,k] == 0)
-    end
+    # Enforced coverage for specific localities
+    @constraint(model, [j in J_prime], u[j] + v[j] == 1)
 
-    # 6. Coverage and MMT-VC relationships
-    for l in I
-        for k in 1:M
-            @constraint(model, sum(z[l+n, i, k] for i in N) <= y[l])
-            @constraint(model, sum(z[l+n, i, k] for i in N) <= delta[k])
-            @constraint(model, sum(z[l+n, i, k] for i in N) >= y[l] + delta[k] - 1)
-        end
-    end
+    @expression(model, sum_delta, sum(delta))
 
-    # 7. Ensure z is only active if MMT is used
-    for i in N, j in N, k in 1:M
-        @constraint(model, z[i, j, k] <= delta[k])
-    end
+    # Budget constraint
+    @constraint(model,
+        sum(D .* sum(z[:,:,k] for k in 1:M)) +
+        sum_delta * p +
+        sum(y .* f) <= B
+    )
 
-    # 8. Cost constraint
-    @constraint(model, sum(q[j] * u[j] for j in J) <= sum(C[i] * y[i] for i in I))
-
-    # 9. Enforced coverage for specific localities
-    for j in J_prime
-        @constraint(model, x[j] == 1)
-    end
-
-    # 10. Budget constraint
-    @constraint(model, (sum(z[i,j,k]*D[i,j] for k in 1:M, i in N, j in N)) + (sum(delta[k] for k in 1:M) * p) + sum(y[i] * f[i] for i in I) <= B)
+    #Additional constraint
+    @constraint(model, sum_delta >= sum_uq/Q)
 
 
     optimize!(model)
-
     return model
 end
 
