38868bd1f1739a5f9afa5b2b72be0b5fd1115992
TheoHanon <99546301+TheoHanon@users.noreply.github.com>
2024-11-20 10:37:35 +0100

    Add new project file with greedy optimization and local search implementation for MMTs

---
+++ project.jl
@@ -0,0 +1,80 @@
+include("model.jl")
+include("Parser.jl")
+include("greedy.jl")
+include("utils.jl")
+using JuMP
+using HiGHS
+
+
+n, m, x_VC, y_VC, x_loc, y_loc, Q, C, q, f, p, tc, B, R, localities_with_high_priorities, M = parse_instance("Combinatorial-Optimization/Instances/coord50-5-1_processed.dat")
+
+
+
+println("Budget: ", B)
+println("Q = ", Q)
+println("C = ", C)
+println("M = ", M)
+
+
+# Create the distance matrix
+distance_matrix = zeros(Float64, m, n)
+
+for i in 1:m 
+    for j in 1:n
+        distance_matrix[i, j] = sqrt((x_VC[i] - x_loc[j])^2 + (y_VC[i] - y_loc[j])^2)
+    end
+end
+
+
+# Create reachability matrix a
+A = zeros(Int, m, n)
+
+
+for i in 1:m
+    for j in 1:n
+        if distance_matrix[i, j] <= R[i]
+            A[i, j] = 1
+        end
+    end
+end
+
+
+D = zeros(Float64, n+m, n+m)
+#The n first rows/cols of D relate to the localities, the m last to the VC
+#In other words, calling D[i,j] for i in I, j in J corresponds to call D[i+n,j]
+for i in 1:m+n
+    for j in 1:m+n
+        if i <= n && j <= n
+            # Both indices in `loc`
+            D[i, j] = sqrt((x_loc[i] - x_loc[j])^2 + (y_loc[i] - y_loc[j])^2)
+        elseif i > n && j > n
+            # Both indices in `VC`
+            D[i, j] = sqrt((x_VC[i - n] - x_VC[j - n])^2 + (y_VC[i - n] - y_VC[j - n])^2)
+        elseif i > n && j <= n
+            # i in `VC`, j in `loc`
+            D[i, j] = sqrt((x_VC[i - n] - x_loc[j])^2 + (y_VC[i - n] - y_loc[j])^2)
+        else
+            # i in `loc`, j in `VC`
+            D[i, j] = sqrt((x_loc[i] - x_VC[j - n])^2 + (y_loc[i] - y_VC[j - n])^2)
+        end
+    end
+end
+
+
+VC, MMTs, Budget, Q_MMTs, Q_tot = greedy_OptVax(n, m, D, A, Q, C, q, f, p, B, R, localities_with_high_priorities, M)
+new_MMTs = local_search_2Opt(MMTs, D)
+
+
+plot_solution_greedy(MMTs, x_VC, y_VC, x_loc, y_loc, R, n, m, M, localities_with_high_priorities)
+plot_solution_greedy(new_MMTs, x_VC, y_VC, x_loc, y_loc, R, n, m, M, localities_with_high_priorities)
+
+
+model_OptVax1 = solve_OptVax1(n, m, D, A, Q, C, q, f, p, B, R, localities_with_high_priorities, M)
+greedy_init(model_OptVax1, new_MMTs, VC, M, n, m, q)
+optimize!(model_OptVax1)    
+
+obj_val = objective_value(model_OptVax1)
+println("Objective Value: ", obj_val)
+
+
+plot_solution(model_OptVax1, x_VC, y_VC, x_loc, y_loc, R,n,m,M, localities_with_high_priorities)
